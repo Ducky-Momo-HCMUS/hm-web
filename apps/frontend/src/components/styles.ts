f1a7007bbc48cddc8806c3df280598b9d47730d6
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2022-10-23 17:06:17 +0700

    [HM-18] Fix styling error

--- apps/frontend/src/components/styles.ts
+++ apps/frontend/src/components/styles.ts
@@ -3,25 +3,25 @@ import { styled } from '@mui/material/styles';
 import { grey } from '@mui/material/colors';
 
 export const StyledContainer = styled(Box)`
-  background: ${grey[100]},
-  height: '100vh'
+  background: ${grey[100]};
+  height: 100vh;
 `;
 
 export const StyledCard = styled(Box)`
-  background: ${grey[50]},
-  borderRadius: '0.625rem',
-  maxWidth: '24rem',
-  height: 'fit-content',
-  boxShadow:
-    'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
-  padding: '3rem 2rem',
-  margin: '3rem auto 0 auto',
+  background: ${grey[50]};
+  border-radius: 0.625rem;
+  max-width: 24rem;
+  height: fit-content;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  padding: 3rem 2rem;
+  margin: 3rem auto 0 auto;
 `;
 
 export const StyledLogo = styled(Avatar)`
-  width: '9rem',
-  height: '7.5rem',
-  margin: '0 auto 2rem auto',
+  width: 9rem;
+  height: 7.5rem;
+  margin: 0 auto 2rem auto;
 `;
 
 export const StyledTextField = styled(TextField)`
