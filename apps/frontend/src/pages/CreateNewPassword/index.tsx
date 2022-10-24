ae38d1f37b7ce49fe0b991ea25bcc217ee43465e
Tam Nguyen <58521730+nntt1210@users.noreply.github.com>
2022-10-24 21:58:22 +0700

    [HM-27][UI] Add reset password pages (#6)
    * [HM-27] Add reset password pages
    
    * [HM-27] Fix linting errors

--- apps/frontend/src/pages/CreateNewPassword/index.tsx
+++ apps/frontend/src/pages/CreateNewPassword/index.tsx
@@ -1,14 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { VisibilityOff, Visibility } from '@mui/icons-material';
-import {
-  Box,
-  Button,
-  Checkbox,
-  Grid,
-  IconButton,
-  InputAdornment,
-  Link,
-} from '@mui/material';
+import { Box, Button, IconButton, InputAdornment } from '@mui/material';
 import Header from '../../components/Header';
 import {
   StyledCard,
@@ -16,20 +8,21 @@ import {
   StyledLogo,
   StyledTextField,
 } from '../../components/styles';
-import { StyledFormControlLabel } from './styles';
 import ErrorMessage from '../../components/ErrorMessage';
 
 interface State {
-  username: string;
   password: string;
+  confirmPassword: string;
   showPassword: boolean;
+  showConfirmPassword: boolean;
 }
 
-function Login() {
+function CreateNewPassword() {
   const [values, setValues] = useState<State>({
-    username: '',
     password: '',
+    confirmPassword: '',
     showPassword: false,
+    showConfirmPassword: false,
   });
 
   const handleChange = useCallback(
@@ -46,19 +39,26 @@ function Login() {
     }));
   }, []);
 
+  const handleClickShowConfirmPassword = useCallback(() => {
+    setValues((v) => ({
+      ...v,
+      showConfirmPassword: !v.showConfirmPassword,
+    }));
+  }, []);
+
   const [error, setError] = useState<string>('');
 
   const handleSubmit = useCallback(
     (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      if (!values.username) {
-        setError('Tài khoản không hợp lệ');
+      if (values.password !== values.confirmPassword) {
+        setError('Mật khẩu không khớp');
         return;
       }
 
       setError('');
     },
-    [values.username]
+    [values.confirmPassword, values.password]
   );
 
   return (
@@ -73,14 +73,7 @@ function Login() {
         {error && <ErrorMessage content={error} />}
         <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
           <StyledTextField
-            label="Tên đăng nhập"
-            name="username"
-            sx={{ margin: '0.5rem 0', width: '100%' }}
-            variant="filled"
-            onChange={handleChange('username')}
-          />
-          <StyledTextField
-            label="Mật khẩu"
+            label="Mật khẩu mới"
             name="password"
             sx={{ margin: '0.5rem 0', width: '100%' }}
             type={values.showPassword ? 'text' : 'password'}
@@ -105,22 +98,35 @@ function Login() {
             }}
             variant="filled"
           />
-          <Grid container alignItems="center">
-            <Grid item xs>
-              <StyledFormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Ghi nhớ đăng nhập"
-              />
-            </Grid>
-            <Grid item>
-              <Link href="/reset-password" variant="body2">
-                Quên mật khẩu?
-              </Link>
-            </Grid>
-          </Grid>
+          <StyledTextField
+            label="Nhập lại mật khẩu mới"
+            name="confirmPassword"
+            sx={{ margin: '0.5rem 0', width: '100%' }}
+            type={values.showConfirmPassword ? 'text' : 'password'}
+            value={values.confirmPassword}
+            onChange={handleChange('confirmPassword')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowConfirmPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? (
+                      <VisibilityOff sx={{ fontSize: '1.25rem' }} />
+                    ) : (
+                      <Visibility sx={{ fontSize: '1.25rem' }} />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            variant="filled"
+          />
           <Box sx={{ textAlign: 'right' }}>
             <Button type="submit" variant="contained" sx={{ mt: 1 }}>
-              Đăng nhập
+              Lưu thay đổi
             </Button>
           </Box>
         </Box>
@@ -129,4 +135,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default CreateNewPassword;
