8f633eb59163478309e09e152b77e0d6c58d117e
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2022-12-09 15:36:30 +0700

    [HM-89] Update styling

--- apps/frontend/src/pages/ClassDetail/ClassInfo/styles.ts
+++ apps/frontend/src/pages/ClassDetail/ClassInfo/styles.ts
@@ -4,7 +4,7 @@ import { styled } from '@mui/material/styles';
 export const StyledTitle = styled(Typography)(({ theme }) => ({
   fontSize: '1.25rem',
   fontWeight: 'bold',
-  color: theme.palette.primary.dark,
+  color: theme.palette.primary.main,
 }));
 
 export const StyledContainer = styled(Box)`
