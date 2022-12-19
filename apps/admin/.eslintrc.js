3de68d7debd9c45aec0f3a5ffbe5cf83d9120010
phineasla <63248715+phineasla@users.noreply.github.com>
2022-12-19 23:19:38 +0700

    [HM-0] Setup admin

---
+++ apps/admin/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['../../.eslintrc.json'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    'react/no-unused-prop-types': 0,
+    'react/jsx-no-useless-fragment': 0,
+    'prettier/prettier': 1,
+    'no-nested-ternary': 1,
+    'react/require-default-props': 0,
+    'no-param-reassign': 0,
+    'react/no-array-index-key': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-console': 0,
+  },
+  ignorePatterns: ['**/generated-types.ts'],
+};
