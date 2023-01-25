fdfd447d44c8f6c7cb5359deb23539bff2ea83fe
Tam Nguyen <58521730+nntt1210@users.noreply.github.com>
2023-01-25 10:53:46 +0700

    [HM-156] Initialize academic staff app (#32)

---
+++ apps/academic-staff/src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import { blue } from '@mui/material/colors';
+import { styled } from '@mui/material/styles';
+
+export const StyledBox = styled(Box)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  background: blue[900],
+  padding: '0 1.25rem',
+  height: '4rem',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}));
