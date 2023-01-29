f6206bbe03c65055c6856556f0afaef87ecb2c5f
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2023-01-29 14:38:08 +0700

    [HM-157] Initialize import page

---
+++ apps/academic-staff/src/pages/Home/ImportFile/styles.ts
@@ -0,0 +1,15 @@
+import { FormControl } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const StyledFormControl = styled(FormControl)`
+  min-width: 7.5rem;
+  &:first-child {
+    margin-right: 1rem;
+  }
+  & > div {
+    background-color: white;
+  }
+  & > div > div {
+    padding: 0.875rem;
+  }
+`;
--- apps/academic-staff/src/pages/Home/index.tsx
+++ apps/academic-staff/src/pages/Home/index.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { List } from '@mui/material';
+import PublishIcon from '@mui/icons-material/Publish';
 
-export default function Home() {
-  return <div>Home</div>;
+import Header from '../../components/Header';
+
+import ImportFile from './ImportFile';
+import {
+  Drawer,
+  StyledContent,
+  StyledListItemButton,
+  StyledListItemText,
+  StyledContainer,
+} from './styles';
+
+function Home() {
+  const [selected, setSelected] = useState(0);
+
+  return (
+    <>
+      <Header isAuthenticated />
+      <StyledContainer>
+        <Drawer variant="permanent" open PaperProps={{ elevation: 8 }}>
+          <List component="nav">
+            <StyledListItemButton
+              active={selected === 0}
+              onClick={() => setSelected(0)}
+            >
+              <PublishIcon color="action" />
+              <StyledListItemText primary="Nhập thông tin" />
+            </StyledListItemButton>
+          </List>
+        </Drawer>
+        <StyledContent component="main">
+          {selected === 0 && <ImportFile />}
+        </StyledContent>
+      </StyledContainer>
+    </>
+  );
 }
+
+export default Home;
